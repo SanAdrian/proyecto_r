2141dcf0db5ac71812b0286aa0454ae305cb741e
SanAdrian <fredyadriansanchez@gmail.com>
2019-10-07 22:54:52 -0300

    coso

---
+++ admin/coso.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Proyecto R</title>
+<?php
+include "php/link.php";
+?>   
+
+</head>
+<body>
+<?php
+include "componentes/topheader.php";
+include "componentes/mobilemenu.php";
+include "componentes/navbar.php"
+?>
+<!-- Insertat contenido -->
+
+
+
+<!-- Fin Insertar contenido -->
+
+<?php
+include "php/script.php"
+?>
+</body>
+</html>
